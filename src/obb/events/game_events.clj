bbb51abe6df3b93f50b5895b157a256da57242d4
Pedro Santos <donbonifacio@gmail.com>
2016-04-17 15:31:32 +0100

    Refactor game-events and remove subscribe to event
    Motivation:
    
    After thinking on the overal flow of the game we came to realize that
    subscribing to games based on events wasn't something useful.
    
    Modifications:
    
    Refactored game-events to be specific to games being updated. We can
    subscribe to all games or just to a specific game.

--- src/obb/events/game_events.clj
+++ src/obb/events/game_events.clj
@@ -6,47 +6,36 @@
             [obb.models.game-info :as game-info]))
 
 (defprotocol GameEvents
-  (publish [this event-name game-info])
-  (subscribe-event [this event-name subscriber-ch])
+  (publish [this game-info])
   (subscribe-game [this game-info subscriber-ch])
   (subscribe-all [this subscriber-ch]))
 
-(defrecord GameEventSystem [master-ch event-publication game-publication broadcast]
+(defrecord GameEventSystem [master-ch game-publication multiplier]
 
   component/Lifecycle
 
   (start [component]
     (let [master-ch (async/chan)
-          broadcast (async/mult master-ch)
-          event-publisher-ch (async/chan)
+          multiplier (async/mult master-ch)
           game-publisher-ch (async/chan)]
-      (async/tap broadcast event-publisher-ch)
-      (async/tap broadcast game-publisher-ch)
+      (async/tap multiplier game-publisher-ch)
       (assoc component :master-ch master-ch
-                       :event-publication (async/pub event-publisher-ch :event-name)
                        :game-publication (async/pub game-publisher-ch :game-id)
-                       :broadcast broadcast)))
+                       :multiplier multiplier)))
 
   (stop [component]
     (async/close! master-ch)
     (-> component
-        (dissoc :event-publication)
         (dissoc :game-publication)
-        (dissoc :broadcast)))
+        (dissoc :multiplier)))
 
   GameEvents
 
-  (publish [this event-name game-info]
+  (publish [this game-info]
     (async/go
       (async/>! master-ch
-                {:event-name event-name
-                 :game-id (game-info/id game-info)
+                {:game-id (game-info/id game-info)
                  :data game-info})))
-
-  (subscribe-event [this event-name subscriber-ch]
-    (async/sub event-publication
-               event-name
-               subscriber-ch))
 
   (subscribe-game [this game-info subscriber-ch]
     (async/sub game-publication
@@ -54,7 +43,7 @@
                subscriber-ch))
 
   (subscribe-all [this subscriber-ch]
-    (async/tap broadcast subscriber-ch)))
+    (async/tap multiplier subscriber-ch)))
 
 (defn create
   "Creates a new component for game events"
--- test/obb/events/game_events_test.clj
+++ test/obb/events/game_events_test.clj
@@ -21,29 +21,20 @@
   (let [system (-> (game-events-system) component/start)
         game-events (:game-events system)
         game-info (game-info/create)
-        subscriber-ch (async/chan 1)
+        game-ch (async/chan 1)
         subscriber-all-ch (async/chan 1)
         test-event :new-game
-        expected-message {:event-name test-event
-                          :game-id (game-info/id game-info)
+        expected-message {:game-id (game-info/id game-info)
                           :data game-info}]
 
-    (testing "subscribe to a specific event and to all events
-             and publish an event"
-      (game-events/subscribe-event game-events :new-game subscriber-ch)
+      (game-events/subscribe-game game-events game-info game-ch)
       (game-events/subscribe-all game-events subscriber-all-ch)
-      (game-events/publish game-events :new-game game-info)
+      (game-events/publish game-events game-info)
 
       (testing "and both subscribers should receive the event"
-        (let [ev1 (async/<!! subscriber-ch)
+        (let [ev1 (async/<!! game-ch)
               ev2 (async/<!! subscriber-all-ch)]
           (is (= ev1 ev2))
-          (is (= ev1 expected-message)))))
-
-    (testing "subscribe to a specific game"
-      (let [game-ch (async/chan)]
-        (game-events/subscribe-game game-events game-info game-ch)
-        (game-events/publish game-events :new-game game-info)
-        (is (= expected-message (async/<!! game-ch)))))
+          (is (= ev1 expected-message))))
 
     (component/stop system)))
